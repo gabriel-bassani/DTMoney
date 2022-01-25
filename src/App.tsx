42ec0158e1d29a4e6086c81bc8d9694f28281668
Bassani <gabrielbassabarreto@gmail.com>
2022-01-24 23:42:48 -0300

    adicao de context

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 import { useState } from 'react';
 import { NewTransactionModal } from "./components/NewTransactionModal";
+import { transactionsContext } from "./TransactionsContext";
 
 
 export function App() {
@@ -15,13 +16,13 @@ export function App() {
         setIsNewTransactionModalOpen(false)
     }
   return (
-    <>
+    <transactionsContext.Provider value={[]}>
       <Header handleOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard/>
       <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal}/>
       <GlobalStyle/>
 
-    </>
+    </transactionsContext.Provider>
   );
 }
 
